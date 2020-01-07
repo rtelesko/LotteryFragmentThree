8991e6338129fc446a257537f33055284c1bf772
rainer <rainer.telesko@fhnw.ch>
2020-01-07 09:16:24 +0100

    Initial commit

---
+++ app/src/main/java/com/example/lotteryfragmentthree/MyListFragment.java
@@ -0,0 +1,36 @@
+package com.example.lotteryfragmentthree;
+
+import android.app.ListFragment;
+import android.os.Bundle;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
+
+public class MyListFragment extends ListFragment implements OnItemClickListener {
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.list_fragment, container, false);
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(getActivity(),
+                R.array.players, android.R.layout.simple_list_item_1);
+        setListAdapter(adapter);
+        getListView().setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Toast.makeText(getActivity(), "Item at position " + position + ": " + getListView().getItemAtPosition(position).toString(), Toast.LENGTH_LONG).show();
+    }
+}
